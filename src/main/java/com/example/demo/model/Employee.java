065b26e59fea9138b80ace4ee4e91c9126afcee2
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-18 11:49:24 +0530

    Update Employee.java

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,22 +1,28 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
+@Table(name="Employee")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Employee {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="Employee_ID")
 	private int Id;
+	@Column(name="Employee_Name")
 	private String Name;
+	@Column(name="Employee_Salary")
 	private int Salary;
 	public int getId() {
 		return Id;
